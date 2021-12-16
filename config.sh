01a1b877985d7713d1a8fb35b3af27ec0c25824c
pakmxy <96231610+pakmxy@users.noreply.github.com>
2021-12-16 10:11:36 +0000

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# config v2ray as
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}		
+EOF
+
+# enable v2ray 
+/usr/bin/v2ray -config /etc/config.json
